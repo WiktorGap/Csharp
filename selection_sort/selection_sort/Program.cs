707800aa97392eb8c3cb120bd65318b939a9ba68
WiktorGap <148978926+WiktorGap@users.noreply.github.com>
2025-01-02 22:14:27 +0100

    Add files via upload

---
+++ selection_sort/selection_sort/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace SelectionSort
+{
+
+    class Program 
+    {
+    
+       static void Main(string[] args)
+        {
+            int[] arr = { 64, 25, 12, 22, 11 };
+            SelectionSort(arr);
+            Console.WriteLine("Sorted array");
+            PrintArray(arr);
+        }
+
+
+        static void PrintArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void SelectionSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[min_idx])
+                    {
+                        min_idx = j;
+                    }
+                }
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+    }
+
+
+    
+
+
+
+}
+
+

38f520f635e40b67b38a9a6ec7cd39608de1652f
WiktorGap <148978926+WiktorGap@users.noreply.github.com>
2025-04-02 11:41:44 +0200

    Add files via upload

---
+++ validPeselApp/validPeselApp/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace validPeselApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValidatePesel = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtValidatePesel
+            // 
+            this.txtValidatePesel.Location = new System.Drawing.Point(227, 107);
+            this.txtValidatePesel.Name = "txtValidatePesel";
+            this.txtValidatePesel.Size = new System.Drawing.Size(100, 20);
+            this.txtValidatePesel.TabIndex = 0;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(372, 103);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtValidatePesel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValidatePesel;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ validPeselApp/validPeselApp/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace validPeselApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int[] processPesel(string pesel)
+        {
+
+            int lenghtOfPesel = pesel.Length;
+
+            if (lenghtOfPesel != 11)
+            {
+                MessageBox.Show("Invalid length of Pesel");
+                return null;
+            }
+
+            // string pattern = @"^\d{2}([0-1]\d|2[0-9]|3[0-2]|4[0-9]|5[0-2]|6[0-9]|7[0-2])[0-3]\d{4}[0-9]$";
+
+            /*
+            if (!Regex.IsMatch(pesel, pattern))
+            {
+                MessageBox.Show("Invalid PESEL format.");
+                return;
+            }*/
+
+
+            int[] intsPesel = new int[lenghtOfPesel];
+
+            for (int i = 0; i < lenghtOfPesel; i++)
+            {
+                if (char.IsDigit(pesel[i]))
+                {
+                    intsPesel[i] = pesel[i] - '0';
+                }
+                else
+                {
+                    MessageBox.Show("PESEL zawiera niepoprawne znaki.");
+                    return null;
+                }
+            }
+            //MessageBox.Show("Tablica wartości PESEL: " + string.Join(", ", intsPesel));
+            return intsPesel;
+
+            
+        }
+
+
+
+    private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string pesel = txtValidatePesel.Text;
+            int[] peselArr = processPesel(pesel);
+            int controlSum = 0;
+            int[] valArr = {1,3,7,9,1,3,7,9,1,3 };
+            
+
+            if (peselArr == null) 
+            {
+                MessageBox.Show("Błąd w przetwarzaniu PESEL.");
+                return;
+            }
+
+            for (int i = 0;i <valArr.Length; i++) 
+            {
+                controlSum += valArr[i] * peselArr[i];
+            }
+
+            int controlNum = 10 - (controlSum % 10);
+            if (controlNum == 10) controlNum = 0; 
+
+            if (controlNum == peselArr[10])
+            {
+                MessageBox.Show("PESEL jest poprawny, liczba kontrolna się zgadza.");
+            }
+            else
+            {
+                MessageBox.Show("Niepoprawna cyfra kontrolna w PESEL.");
+            }
+
+
+        }
+    }
+}
